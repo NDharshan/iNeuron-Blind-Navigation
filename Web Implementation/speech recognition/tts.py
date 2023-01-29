e5b6b3929ed23fedc7066138d1ebe644b0c56ea6
NDharshan <98468801+NDharshan@users.noreply.github.com>
2023-01-29 16:20:20 +0530

    Delete Web Implementation directory

--- Web Implementation/speech recognition/tts.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-import os
-
-mytext = "Hi, this is an example of converting text to audio. This is a bot speaking here, not a real human!"
-audio = gTTS(text=mytext, lang="en", slow=False)
-
-audio.save("example.mp3")
-os.system("start example.mp3")
